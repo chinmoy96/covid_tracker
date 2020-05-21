40e2f4e161d9777668fe62006ea0d5943aef1a2a
chinmoy96 <chinmoy.pradhan4@gmail.com>
2020-05-22 02:06:18 +0530

    project initialised

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { Cards } from './components'
+class App extends React.Component {
+  
+  
+  
+  
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+      </div>
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
 
 export default App;
